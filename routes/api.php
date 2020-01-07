c9e34b0f562aa5e1036271d24371e0ddd90ad01b
osos19995 <geomohamed19995@gmail.com>
2020-01-07 19:41:53 +0200

    =before complete

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group([
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('register', 'AuthController@Register');
+
 });
 Route::resource('categories','ApiCategoriesController');
 Route::resource('subcategories','ApiSubcategoriesController');
